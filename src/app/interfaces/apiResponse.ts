764cea662a12fc438a7b5baf3f02f42d91b65e40
mangiu90 <giudicimanuel@gmail.com>
2023-07-18 20:04:05 -0300

    create interfaces

---
+++ src/app/interfaces/apiResponse.ts
@@ -0,0 +1,13 @@
+export interface ImageResponse
+{
+    message: string,
+    status: string
+}
+
+export interface JokeResponse
+{
+    type: string,
+    setup: string,
+    punchline: string,
+    id: number,
+}
